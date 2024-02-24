1c67f2e2f6033ba5f066d624a7c2437469aa0cf2
FIRSTstar12 <143663224+FIRSTstar12@users.noreply.github.com>
2024-02-24 14:25:47 +0000

    Make it able to use only one input line! A first for me.

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-num1 = float(input())
-num2 = float(input())
-symbol = input()
-if symbol == "+":
-  add = num1 + num2
-elif symbol == "-":
-  add = num1 - num2
-elif symbol == "*":
-  add = num1 * num2
-elif symbol == "/":
-  add = num1 / num2
-else:
-  print("Invalid Input")
-print(add)
+equation = input()
+spltEquation = equation.split(' ')
+equation = []
+symbols = []
+for i in spltEquation:
+  if i.strip('-').isdigit():
+    equation.append(int(i))
+  else:
+    symbols.append(i)
+for i in symbols:
+  if i == '+':
+    equation.append(equation.pop(0) + equation.pop(0))
+  elif i == '-':
+    equation.append(equation.pop(0) - equation.pop(0))
+  elif i == '*':
+    equation.append(equation.pop(0) * equation.pop(0))
+  elif i == '/':
+    equation.append(equation.pop(0) / equation.pop(0))
+  else:
+    print('Invalid equation')
+    break
+  
+print(f"The answer is {equation}")
